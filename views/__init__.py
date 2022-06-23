fe0db235bdd8b6f9e5146e024123c277f5c0e002
Sebastian-github-2020 <zaksvisitor@gmail.com>
2022-06-23 14:18:00 +0800

    上传文件

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,6 @@
 from .index import index_router
 from .user import user_router
 from .news import news_router
+from .upload import upload_router
 
-__all__ = ['index_router', 'user_router', 'news_router']
+__all__ = ['index_router', 'user_router', 'news_router', 'upload_router']
---
+++ views/upload.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, File, UploadFile
+
+upload_router = APIRouter()
+
+
+@upload_router.post("/")
+async def upload_file(file: UploadFile = File(..., media_type="*")):
+    res = await file.read()
+    with open("upload/" + file.filename, 'wb+') as f:
+        f.write(res)
+    return {"file_info": file.filename, "content_type": "content_type", "status": "文件上传成功"}
